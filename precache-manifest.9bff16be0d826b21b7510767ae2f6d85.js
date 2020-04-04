995763d8242fa8f2e92c0664a044abaa63c2754f
pranayag <agarwalpranay76161@gmail.com>
2020-04-04 14:00:25 +0530

    Updates

---
+++ precache-manifest.9bff16be0d826b21b7510767ae2f6d85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5560b2e7b0b71036a34d1eee10ff777d",
+    "url": "/Quoter/index.html"
+  },
+  {
+    "revision": "b3e63e57a68993f04f3f",
+    "url": "/Quoter/static/css/main.4c281c24.chunk.css"
+  },
+  {
+    "revision": "36af871a14ed84fe0a06",
+    "url": "/Quoter/static/js/2.0e219b83.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Quoter/static/js/2.0e219b83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3e63e57a68993f04f3f",
+    "url": "/Quoter/static/js/main.ba252acb.chunk.js"
+  },
+  {
+    "revision": "c1d38317af8a1aa089b1",
+    "url": "/Quoter/static/js/runtime-main.9570b641.js"
+  }
+]);
